ae240f486662ea4c10fc306df2824ac62a240538
anaesperanzalin <anaesperanzalin@users.noreply.github.com>
2020-05-31 14:44:26 -0700

    Setting up Github pages

---
+++ build/precache-manifest.f51ab5def036db8c3d0ce1d4b2a84d04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f554137b8a6b8f48f7e",
+    "url": "/myreads2/static/css/main.cbb129e9.chunk.css"
+  },
+  {
+    "revision": "2f554137b8a6b8f48f7e",
+    "url": "/myreads2/static/js/main.2f554137.chunk.js"
+  },
+  {
+    "revision": "319d0ecd238cc8ff7a84",
+    "url": "/myreads2/static/js/1.319d0ecd.chunk.js"
+  },
+  {
+    "revision": "607a6df4d2b260a57ebc",
+    "url": "/myreads2/static/js/runtime~main.607a6df4.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads2/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads2/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myreads2/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "b3286949024e021103f4b57acebab2da",
+    "url": "/myreads2/index.html"
+  }
+];
